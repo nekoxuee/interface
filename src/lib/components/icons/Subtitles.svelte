b8cfd0fe8c564f07befd5e19cd1527325e02fb19
nekoxue <43864935+nekoxuee@users.noreply.github.com>
2026-04-23 19:11:02 +0200

    refactor: update subtitle icon to filled lucide design

--- src/lib/components/icons/Subtitles.svelte
+++ src/lib/components/icons/Subtitles.svelte
@@ -6,7 +6,7 @@
   type Attrs = SVGAttributes<SVGSVGElement>
   const iconNode: Array<[elementName: keyof SvelteHTMLElements, attrs: Attrs]> = [
     ['path', { fill: 'none', d: 'M0 0h24v24H0z' }],
-    ['path', { d: 'M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM4 12h4v2H4v-2zm10 6H4v-2h10v2zm6 0h-4v-2h4v2zm0-4H10v-2h10v2z' }]
+    ['path', { fill: 'currentColor', d: 'M5 4h14a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3zm6 10h-4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2zm6 0h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zm-8-4h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zm8 0h-4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2z' }]
   ]
 </script>
 
