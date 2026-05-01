7dd81c2bff90ac69585d297bfae613099e2b5b2f
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-05-02 01:43:12 +0200

    fix: view more not clickable

--- src/routes/app/home/+page.svelte
+++ src/routes/app/home/+page.svelte
@@ -97,11 +97,11 @@
 <div class='grow h-full min-w-0 -ml-14 pl-14 overflow-y-scroll' use:dragScroll on:scroll={handleScroll}>
   <Banner />
   {#each $sectionQueries as { title, query, variables }, i (i)}
-    <div class='flex px-4 pt-5 items-end cursor-pointer text-muted-foreground'>
+    <div class='flex px-4 pt-5 items-end cursor-pointer text-muted-foreground relative z-[1]'>
       <div class='font-semibold text-lg leading-none select:text-foreground' use:click={() => search(variables)}>{title}</div>
       <div class='ml-auto text-xs select:text-foreground' use:click={() => search(variables)}>View More</div>
     </div>
-    <div class='flex overflow-x-scroll select:-ml-14 select:pl-14 -mt-40 pt-40 -mb-5 pb-5 relative group' use:dragScroll>
+    <div class='flex overflow-x-scroll select:-ml-14 select:pl-14 select:-mt-40 select:pt-40 -mb-5 pb-5 relative group' use:dragScroll>
       <QueryCard {query} />
     </div>
   {/each}
